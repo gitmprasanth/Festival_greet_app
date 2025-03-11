b577e21356719d5d418287822a6f0fe778d92d23
PRASANTH M <151944542+gitmprasanth@users.noreply.github.com>
2025-03-11 17:45:29 +0530

    Added ReadMe

---
+++ README.md
@@ -0,0 +1,18 @@
+# Festival Greeting app
+
+Festival Greeting app with name.
+
+## Screenshots
+
+![Screenshot](C:\Users\aravi\Desktop\festival_greet_app\assets\Screenshot 2025-03-11 172436.png)
+
+
+![Screenshot2](C:\Users\aravi\Desktop\festival_greet_app\assets\Screenshot 2025-03-11 172453.png)
+
+
+> Live: [https://gitmprasanth.github.io/Festival_greet_app/](https://gitmprasanth.github.io/Festival_greet_app/)
+
+
+#### Authors:
+
+- [Prasanth]()
--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -1,14 +1,13 @@
-//   console.dir(window.location.search.split("?name=%20")[1]);
-const url=new URL(window.location.href)
-const name=url.searchParams.get('name')
-console.log(name);
 
-//Display Name on UI
-  const greeterEl=document.querySelector("#greeter")
-if(name){
-    greeterEl.textContent=name
+const greeterNameEl = document.querySelector("#greeter");
 
-}
-else{
-    location.href="http://127.0.0.1:5500/index.html"
+
+const url = new URL(window.location.href);
+const name = url.searchParams.get("name");
+
+if (name) {
+  // Display that value on UI.
+  greeterNameEl.textContent = name;
+} else {
+  location.href = "http://127.0.0.1:5500/greeting.html?name=";
 }
